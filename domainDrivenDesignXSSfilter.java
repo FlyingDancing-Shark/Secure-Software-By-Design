81b2ab1363099f2a0f4d872295dcdde96664462f
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-19 11:56:24 +0800

    Update domainDrivenDesignXSSfilter.java

--- domainDrivenDesignXSSfilter.java
+++ domainDrivenDesignXSSfilter.java
@@ -5,6 +5,8 @@ description
 
 import static org.apache.commons.lang3.Validate.*;
 
+/* first extract the "username" into a separate Class, this is a more precise domain model 
+   to denote "what a user name supposed to look like?" */
 public class Username {
 	
 	// design by length to limit the username size
@@ -14,3 +16,23 @@ public class Username {
 	// design by regular expression to limit the username format
 	private static final String VALID_CHARACTERS = "[A-Za-z0-9_-]+";
 	
+	private final String value;
+	
+	public Username(final String value) {
+		
+		notBlank(value);
+		final String trimmed = value.trim();
+		
+		
+		inclusiveBetween(MINIMUM_LENGTH, MAXIMUM_LENGTH, trimmed.length());
+		
+		
+		matchesPattern(trimmed, VALID_CHARACTERS, "Allowed characters are: %s", VALID_CHARACTERS);
+		
+		this.value = trimmed;
+	}
+	
+	public String value() { return value; }
+}
+
+
