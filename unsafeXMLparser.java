6ab4686ed1a7d5a491927ad058163a07a2c8f8d4
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-22 10:45:31 +0800

    Create unsafeXMLparser.java

---
+++ unsafeXMLparser.java
@@ -0,0 +1,43 @@
+/********************************************
+description
+although this parser is suggested by OWASP community, it's not secure enough, 
+we must (and will see) improve it by add a separate "lexical content scanner" logic 
+( it primarily reject any entities in an XML, return false, before pass it to the parser ) 
+this will be done in our revised version.
+********************************************/
+
+import static javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING;
+
+public final class XMLparser {
+
+	static final String DISALLOW_DOCTYPE = 
+			"http://apache.org/xml/features/disallow-doctype-decl";
+	static final String ALLOW_EXTERNAL_GENERAL_ENTITIES	= 
+			"http://xml.org/sax/features/external-gernel-entities";
+	static final String ALLOW_EXTERNAL_PARAMETER_ENTITIES	= 
+			"http://xml.org/sax/features/external-parameter-entities";"
+	static final String ALLOW_EXTERNAL_DTD =
+			"http://apache.org/xml/features/nonvalidating/load-external-dtd";
+	
+	public static Document parse(final InputStream input)
+										throws SAXException, IOException {
+										
+		try {
+			final DocumentBuilderFactory factory = 
+											DocumentBuilderFactory.newInstance();
+											
+			factory.setExpandEntityReferences(false);
+			factory.setFeature(FEATURE_SECURE_PROCESSING, true);
+			factory.setFeature(DISALLOW_DOCTYPE, true);
+			factory.setFeature(ALLOW_EXTERNAL_GENERAL_ENTITIES, false);
+			factory.setFeature(ALLOW_EXTERNAL_PARAMETER_ENTITIES, false);
+			factory.setFeature(ALLOW_EXTERNAL_DTD, false);
+			
+			retun factory.newDocumentBuilder().parse(input);
+		}
+		catch(ParserConfigurationException e) {
+			throw new IllegalStateException("Configuration Error", e);
+		}
+	}
+	
+}
