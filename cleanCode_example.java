492e75f38b1e34663f24b61ca12c09e722c41e04
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-12-06 09:58:55 +0800

    Update cleanCode_example.java

--- cleanCode_example.java
+++ cleanCode_example.java
@@ -23,3 +23,18 @@ public static String renderPageWithSetupsAndTeardowns(
 }
 
 
+
+public Money calculatePay(Employee e) throws InvalidEmployeeType {
+    switch (e.type) {
+       case COMMISSIONED:
+         return calculateCommissionedPay(e);
+       case HOURLY:
+         return calculateHourlyPay(e);
+       case SALARIED:
+         return calculateSalariedPay(e);
+       default:
+         throw new InvalidEmployeeType(e.type);
+    }
+}
+
+
